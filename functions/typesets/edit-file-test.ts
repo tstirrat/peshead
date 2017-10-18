0c4fdbe797cb578874e126d450a2a08e22680666
Tim Stirrat <tstirrat@google.com>
2017-10-18 09:34:49 -0700

    Update player offsets, test against 2 binaries

--- functions/typesets/edit-file-test.ts
+++ functions/typesets/edit-file-test.ts
@@ -1,42 +1,95 @@
-import {EditFile, HEADER_SIZE_BYTES, PLAYER_SIZE_BYTES} from './edit-file';
+import {EditFile, HEADER_SIZE_BYTES, Player, PLAYER_SIZE_BYTES} from './edit-file';
+
 /// <reference path="../@types/jbinary.d.ts" />
 import jBinary = require('jbinary');
 
-// const fileName = `${__dirname}/../test/mcnameface-lacasweat_EDIT00000000`;
-const fileName = `${__dirname}/../test/N.MCNAMEFACE.bin`;
+const attrs64File = `${__dirname}/../test/player-attrs-64.bin`;
+const attrs99File = `${__dirname}/../test/player-attrs-99-descending.bin`;
 
-// TODO(tstirrat/pesleagues#2): declare/generate types for these
-// let data: any;
-let player: any;
-// let team: any;
+let attrs64Player: Player;
+let attrs99Player: Player;
 
 beforeAll(() => {
-  return jBinary.load(fileName, EditFile).then((jb: jBinary) => {
-    const playerOffset: number = PLAYER_SIZE_BYTES * 13980;
-    player = jb.read('Player', HEADER_SIZE_BYTES + playerOffset);
-    player = jb.read('Player', 0);
-    // player = data.players.find((p: any) => p.name === 'M. MCNAMEFACE');
-  });
+  return jBinary.load(attrs64File, EditFile)
+      .then((jb: jBinary) => {
+        attrs64Player = jb.read('Player', 0);
+        return jBinary.load(attrs99File, EditFile);
+      })
+      .then((jb: jBinary) => {
+        attrs99Player = jb.read('Player', 0);
+      });
 });
 
 test('reads player name', () => {
-  expect(player.name).toEqual('M. MCNAMEFACE');
+  expect(attrs64Player.name).toEqual('M. MCNAMEFACE');
 });
 
 test('reads player kit name', () => {
-  expect(player.printName).toEqual('NAMEY');
+  expect(attrs64Player.printName).toEqual('NAMEY');
 });
 
 test('reads player id', () => {
-  expect(player.id).toEqual(1048576);
+  expect(attrs64Player.id).toEqual(1048576);
 });
 
-test('reads player age', () => {
-  expect(player.age).toEqual(15);
-});
+// test('reads player age', () => {
+//   expect(attrs64Player.abilities.age).toEqual(15);
+// });
 
-test('reads player attributes', () => {
-  expect(player.dribbling).toEqual(64);
-  expect(player.attackingProwess).toEqual(64);
-  expect(player.goalkeeping).toEqual(64);
+test('reads player abilities', () => {
+  // --- Player with all abilities set to 64
+
+  expect(attrs64Player.abilities.attackingProwess).toEqual(64);
+  expect(attrs64Player.abilities.ballControl).toEqual(64);
+  // expect(attrs64Player.abilities.dribbling).toEqual(64);
+  // expect(attrs64Player.abilities.lowPass).toEqual(64);
+  // expect(attrs64Player.abilities.loftedPass).toEqual(64);
+  expect(attrs64Player.abilities.finishing).toEqual(64);
+  // expect(attrs64Player.abilities.placeKicking).toEqual(64);
+  expect(attrs64Player.abilities.swerve).toEqual(64);
+  // expect(attrs64Player.abilities.header).toEqual(64);
+  expect(attrs64Player.abilities.defensiveProwess).toEqual(64);
+  expect(attrs64Player.abilities.ballWinning).toEqual(64);
+  // expect(attrs64Player.abilities.kickingPower).toEqual(64);
+  // expect(attrs64Player.abilities.speed).toEqual(64);
+  // expect(attrs64Player.abilities.explosivePower).toEqual(64);
+  expect(attrs64Player.abilities.strength).toEqual(64);
+  // expect(attrs64Player.abilities.physicalContact).toEqual(64);
+  expect(attrs64Player.abilities.jump).toEqual(64);
+  // expect(attrs64Player.abilities.goalkeeping).toEqual(64);
+  expect(attrs64Player.abilities.catching).toEqual(64);
+  // expect(attrs64Player.abilities.clearing).toEqual(64);
+  // expect(attrs64Player.abilities.reflexes).toEqual(64);
+  // expect(attrs64Player.abilities.coverage).toEqual(64);
+  // expect(attrs64Player.abilities.stamina).toEqual(64);
+
+
+  // --- Player with all abilities descending from 99
+
+  expect(attrs99Player.abilities.attackingProwess).toEqual(99);
+  expect(attrs99Player.abilities.ballControl).toEqual(98);
+  // expect(attrs99Player.abilities.dribbling).toEqual(97);
+  // expect(attrs99Player.abilities.lowPass).toEqual(96);
+  // expect(attrs99Player.abilities.loftedPass).toEqual(95);
+  expect(attrs99Player.abilities.finishing).toEqual(94);
+  // expect(attrs99Player.abilities.placeKicking).toEqual(93);
+  expect(attrs99Player.abilities.swerve).toEqual(92);
+  // expect(attrs99Player.abilities.header).toEqual(91);
+  expect(attrs99Player.abilities.defensiveProwess).toEqual(90);
+  expect(attrs99Player.abilities.ballWinning).toEqual(89);
+  // expect(attrs99Player.abilities.kickingPower).toEqual(88);
+  // expect(attrs99Player.abilities.speed).toEqual(87);
+  // expect(attrs99Player.abilities.explosivePower).toEqual(86);
+  expect(attrs99Player.abilities.strength).toEqual(85);
+  // expect(attrs99Player.abilities.physicalContact).toEqual(84);
+  expect(attrs99Player.abilities.jump).toEqual(83);
+  // expect(attrs99Player.abilities.goalkeeping).toEqual(82);
+  expect(attrs99Player.abilities.catching).toEqual(81);
+  // expect(attrs99Player.abilities.clearing).toEqual(80);
+  // expect(attrs99Player.abilities.reflexes).toEqual(79);
+  // expect(attrs99Player.abilities.coverage).toEqual(78);
+  // expect(attrs99Player.abilities.stamina).toEqual(77);
+
+  // expect(attrs99Player.abilities.weakFootUsage).toEqual(3);
+  // expect(attrs99Player.abilities.weakFootAccuracy).toEqual(1);
 });
--- functions/typesets/edit-file.ts
+++ functions/typesets/edit-file.ts
@@ -69,6 +69,7 @@ export const EditFile = {
 
   },
 
+  // Total size = 188 bytes
   Player: {
     id: 'uint32',
     commentaryId: 'uint32',
@@ -78,65 +79,87 @@ export const EditFile = {
     weight: 'uint8',
     motionGoalCelebration1: 'uint8',
     motionGoalCelebration2: 'uint8',
-    attackingProwess: 'Attribute7',
-    defensiveProwess: 'Attribute7',
-    goalkeeping: 'Attribute7',
-    dribbling: 'Attribute7',
-    motionFreeKick: ['bitfield', 4],
-    finishing: 'Attribute7',
-    lowPass: 'Attribute7',
-    loftedPass: 'Attribute7',
-    header: 'Attribute7',
-    form: 'Motion3',
-    isEdited: 'Bool',
-    swerve: 'Attribute7',
-    catching: 'Attribute7',
-    clearing: 'Attribute7',
-    reflexes: 'Attribute7',
-    injuryResistance: 'Bit2',
-    unknown02: 'Bool',
-    hasBasicSettingMods: 'Bool',
-    bodyControl: 'Attribute7',
-    physicalContact: 'Attribute7',
-    kickingPower: 'Attribute7',
-    explosivePower: 'Attribute7',
-    motionArmMovementDribbling: 'Motion3',
-    hasRegisteredPositionMod: 'Bool',
-    age: 'Age6',
-    registeredPosition: 'Position4',
-    unknown03: 'Bool',
-    playingStyles: ['bitfield', 5],
-    ballControl: 'Attribute7',
-    ballWinning: 'Attribute7',
-    weakFootAccuracy: 'Bit2',
-    jump: 'Attribute7',
-    motionArmMovementRunning: 'Motion3',
-    motionCornerKick: 'Motion3',
-    coverage: 'Attribute7',
-    weakFootUsage: 'Bit2',
-    playablePosition: ['array', 'Bit2', 13],
-    motionHunchingDribbling: 'Bit2',
-    motionHunchingRunning: 'Bit2',
-    motionPenaltyKick: 'Bit2',
-    placeKicking: 'Attribute7',
-    isPlayablePositionChanged: 'Bool',
-    hasAbilityMods: 'Bool',
-    hasPlayerSkillMods: 'Bool',
-    stamina: 'Attribute7',
-    speed: 'Attribute7',
-    hasPlayingStyleMods: 'Bool',
-    hasComPlayingStyleMods: 'Bool',
-    hasMotionMods: 'Bool',
-    isBaseCopy: 'Bool',
-    unknown04: 'Bool',
-    preferredFoot: 'Bool',
-    unknown05: 'Bool',
-    comPlayingStyles: 'Attribute7',
-    playerSkills: ['bitfield', 28],
-    unknown06: 'uint8',
+    // ^ 16 bytes
+    // section = 248 bits = 31 bytes
+    abilities: {
+      hasComPlayingStyleMods: 'Bool',  // guess
+      attackingProwess: 'Attribute7',
+      weakFootAccuracy: 'Bit2',  // guess
+      defensiveProwess: 'Attribute7',
+      unknown02: 'uint16',
+      finishing: 'Attribute7',
+      unknown03: ['bitfield', 24],
+      preferredFoot: 'Bool',  // guess
+      // ^ 64 bits = 8 bytes
+
+      swerve: 'Attribute7',
+      injuryResistance: 'Bit2',
+      catching: 'Attribute7',
+      unknown10: 'uint16',
+      // ^ +32 bits = 12 bytes
+
+      bodyControl: 'Attribute7',
+      isEdited: 'Bool',  // guess
+      hasPlayerSkillMods: 'Bool',
+      strength: 'Attribute7',
+      unknown12: 'uint32',
+      // ^ +48 bits = 18 bytes
+
+      ballControl: 'Attribute7',
+      hasBasicSettingMods: 'Bool',  // guess
+      hasAbilityMods: 'Bool',       // guess
+      ballWinning: 'Attribute7',
+      // ^ +16 bits = 20 bytes
+
+      jump: 'Attribute7',
+      unknown22: ['bitfield', 9],
+      // ^ +16 bits = 22 bytes
+
+      skip: ['skip', 8],
+      // // to be assigned
+      // age: 'Age6',  // guess
+      // coverage: 'Attribute7',
+      // unknown25: 'uint32',
+
+      // placeKicking: 'Attribute7',
+      // stamina: 'Attribute7',
+      // speed: 'Attribute7',
+      // unknown23: 'Age6',
+      // form: 'Motion3',         // guess
+      // hasRegisteredPositionMod: 'Bool',  // guess
+      // motionFreeKick: ['bitfield', 4],
+      // lowPass: 'Attribute7',
+      // loftedPass: 'Attribute7',
+      // header: 'Attribute7',
+      // clearing: 'Attribute7',
+      // reflexes: 'Attribute7',
+      // dribbling: 'Attribute7',
+      // physicalContact: 'Attribute7',
+      // kickingPower: 'Attribute7',
+      // explosivePower: 'Attribute7',
+      // motionArmMovementDribbling: 'Motion3',
+      // registeredPosition: 'Position4',
+      // playingStyles: ['bitfield', 5],
+      // motionArmMovementRunning: 'Motion3',
+      // motionCornerKick: 'Motion3',
+      // weakFootUsage: 'Bit2',
+      // playablePosition: ['array', 'Bit2', 13],
+      // motionHunchingDribbling: 'Bit2',
+      // motionHunchingRunning: 'Bit2',
+      // motionPenaltyKick: 'Bit2',
+      // isPlayablePositionChanged: 'Bool',
+      // hasPlayingStyleMods: 'Bool',
+      // hasMotionMods: 'Bool',
+    },
+
+    isBaseCopy: 'Bool',              // guess
+    comPlayingStyles: 'Attribute7',  //
+    unknown06: ['bitfield', 4],      //
+    playerSkills: ['bitfield', 28],  // = 5 bytes
+
     name: ['string0', 46],
     printName: ['string0', 18],
-    appearance: 'PlayerAppearance',
+    appearance: 'PlayerAppearance',  // = 72 bytes
   },
 
   PlayerAppearance: {
@@ -211,3 +234,132 @@ export const EditFile = {
     shirtNumbers: ['array', 'uint8', 32],
   },
 };
+
+
+
+export interface EditFile {
+  header: Header;
+  players: Player[];
+  teams: Team[];
+  managers: Manager[];
+  competitions?: Competition[];
+  stadiums?: Stadium[];
+  teamRosters?: TeamRoster[];
+}
+
+export interface Header {
+  playerCount: number;
+  teamCount: number;
+  managerCount: number;
+  competitionCount: number;
+  stadiumCount: number;
+  teamPlayerCount: number;
+  gamePlanCount: number;
+}
+
+export interface Player {
+  id: number;
+  commentaryId: number;
+  unknown01: number;
+  nationality: number;
+  height: number;
+  weight: number;
+  motionGoalCelebration1: number;
+  motionGoalCelebration2: number;
+  abilities: {
+    hasComPlayingStyleMods: boolean; attackingProwess: number;
+    weakFootAccuracy: number;
+    defensiveProwess: number;
+    unknown02: number;
+    finishing: number;
+    unknown03: number;
+    preferredFoot: boolean;
+
+    swerve: number;
+    injuryResistance: number;
+    catching: number;
+    unknown10: number;
+
+    bodyControl: number;
+    isEdited: boolean;
+    strength: number;
+    unknown12: number;
+    ballControl: number;
+    unknown04: boolean;
+    ballWinning: number;
+    jump: number;
+    unknown22: number;
+  };
+  isBaseCopy: boolean;
+  comPlayingStyles: number;
+  unknown06: number;
+  playerSkills: number;
+  name: string;
+  printName: string;
+  // appearance: PlayerAppearance;
+}
+
+export interface Team {
+  id: string;
+  manager_id: string;
+  emblem_id: string;
+  stadium_id: string;
+  word1: string;
+  nationality_id: string;
+  callname_id: string;
+  color1r: number;
+  color1g: number;
+  stadium_net_id: number;
+  color2: 'Color24', color1b: number;
+  stadium: {
+    isStadiumNetEnabled: boolean; bits1: number; stadiumGoalType: number;
+    bits2: number;
+    stadiumTurfPattern: number;
+    stadiumSidelineColor: number;
+    stadiumSeatColor: number;
+  };
+  isNameEdited: boolean;
+  isStadiumEdited: boolean;
+  '<bool1>': boolean;
+  isStadiumNameEdited: boolean;
+  '<bool2>': boolean;
+  isStadiumConfigEdited: boolean;
+  isRivalEdited: boolean;
+  isBannerEdited: boolean;
+  '<bool3>': boolean;
+  isStripEdited: boolean;
+  isManagerEdited: boolean;
+  '<bool4>': boolean;
+  '<byte1 strip count?>': number;
+  strips: number[];
+  rivals: number[];
+  bannerEnabled: number[];
+  name: string;
+  scoreboard: string;
+  stadiumText: string;
+  banners: string[];
+}
+
+export interface TeamRoster {
+  teamId: string;
+  assignments: number[];
+  shirtNumbers: number[];
+}
+
+export interface Stadium {
+  id: string;
+  name: string;
+}
+
+export interface Manager {
+  id: number;
+  nationality: number;
+  pictureId: number;
+  unknown01: string;
+  name: string;
+}
+
+export interface Competition {
+  id: number;
+  // TODO: complete this
+}
