dad4407f8556c59f2f2a82293ebd3c23051f0b0f
Tim Stirrat <tstirrat@google.com>
2017-10-24 19:03:26 -0700

    Fix epic types, link shared api.js for usage on FE

--- src/containers/Search/epics.ts
+++ src/containers/Search/epics.ts
@@ -1,12 +1,11 @@
 import 'firebase/firestore'; // required for side-effects
 
 import * as firebase from 'firebase';
-import {ActionsObservable, Epic} from 'redux-observable';
-import {map} from 'rxjs/operators/map';
+import {Epic} from 'redux-observable';
 import {switchMap} from 'rxjs/operators/switchMap';
 
-import {Player} from '../../../functions/service/api';
 import {State as GlobalState} from '../../reducers';
+import {Player} from '../../shared/service/api';
 
 import * as search from './actions';
 
@@ -19,20 +18,16 @@ firebase.initializeApp({
 // Initialize Cloud Firestore through Firebase
 const db = firebase.firestore();
 
-// Epic<search.SearchRequestAction, GlobalState>
-export const doSearch =
-    (action$: ActionsObservable<search.SearchRequestAction>) =>
-        action$.ofType(search.SEARCH_REQUEST)
-            .pipe(switchMap((action: search.SearchRequestAction) => {
-              // const {term} = action.payload;
-              return db.collection('players')
-                  .get()
-                  .then(
-                      snapshot =>
-                          snapshot.docs.map(d => Player.create(d).toJSON()))
-                  .then(players => new search.SearchSuccessAction({
-                    results: players,
-                  }))
-                  .catch(err => new search.SearchErrorAction(err));
-
-            }));
+export const doSearch: Epic<search.Actions, GlobalState> = action$ =>
+    action$.ofType(search.SEARCH_REQUEST)
+        .pipe(switchMap((action: search.SearchRequestAction) => {
+          // const {term} = action.payload;
+          return db.collection('players')
+              .get()
+              .then(snapshot => snapshot.docs)
+              .then(docs => docs.map(d => Player.create(d).toJSON()))
+              .then(players => new search.SearchSuccessAction({
+                results: players,
+              }))
+              .catch(err => new search.SearchErrorAction(err));
+        }));
