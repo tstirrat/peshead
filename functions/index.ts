b802410ed07b21776c7e8cac4cf4eb477317fa54
Tim Stirrat <tstirrat@google.com>
2017-11-04 23:10:15 +0100

    feat(functions) elasticsearch: add/remove index and basic search

--- functions/index.ts
+++ functions/index.ts
@@ -1,10 +1,13 @@
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 
+import {addPlayer, createClient, removePlayer, search} from './search';
 import {API, GetPlayerRequest, Player} from './service/api';
 
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
+
+// API
 
 exports.getPlayer = functions.https.onRequest(async (request, response) => {
   const api = API.create((method, request, callback) => {
@@ -23,4 +26,66 @@ exports.getPlayer = functions.https.onRequest(async (request, response) => {
   });
   console.log('created rpc player', rpcPlayer.toJSON());
   response.send(JSON.stringify(player, null, 2));
+});
+
+// SEARCH
+
+enum IndexingState {
+  INDEXING = 'INDEXING',
+  INDEXED = 'INDEXED',
+  ERROR = 'ERROR',
+}
+
+/** Checks for changes to players and updates/removes the search index entry */
+exports.updatePlayerIndex =
+    functions.firestore.document('players/{id}').onWrite(async (event) => {
+      const player = event.data.data();
+      const playerId: string = event.params!.id;
+      const client = createClient(functions.config().es);
+
+      // removal
+      if (player === null) {
+        console.log('[search] removing player', playerId);
+        return await removePlayer(client, playerId);
+      }
+
+      // prevent infinite loop
+      if (player.indexState) {
+        console.info('[search] player already indexed/indexing', playerId);
+        return;
+      }
+
+      // add to index
+      console.log('[search] indexing player', playerId);
+      await event.data.ref.update({indexState: IndexingState.INDEXING});
+      try {
+        await addPlayer(client, playerId, player);
+        await event.data.ref.update({indexState: IndexingState.INDEXED});
+        console.log('[search] indexing complete', playerId);
+      } catch (e) {
+        console.error('[search] indexing error', e);
+        return event.data.ref.update({
+          indexState: IndexingState.ERROR,
+          indexError: e.message,
+        });
+      }
+      return;
+    });
+
+/** Perform a search, mounted at /api/v1/search */
+exports.search = functions.https.onRequest(async (req, res) => {
+  const query: string = req.query.query;
+  if (query) {
+    try {
+      console.log('[search] searching:', query);
+      const client = createClient(functions.config().es);
+      const response = await search(client, query);
+      console.log('[search] search complete', response);
+      return res.send(response);
+    } catch (e) {
+      return res.status(400).send(e);
+    }
+  }
+  console.warn('[search] search with empty query');
+  return res.send({});
 });
---
+++ functions/search.ts
@@ -0,0 +1,82 @@
+import * as AWS from 'aws-sdk';
+import * as elasticsearch from 'elasticsearch';
+import * as functions from 'firebase-functions';
+import * as HttpAmazonESConnector from 'http-aws-es';
+
+import {Player} from './service/api';
+
+interface ClientConfig {
+  access_key_id: string;
+  secret_access_key: string;
+  region?: string;
+  host: string;
+}
+
+export function createClient(config: ClientConfig) {
+  const accessKeyId: string = config.access_key_id;
+  const secretAccessKey: string = config.secret_access_key;
+  const region: string = config.region || 'us-west-1';
+  const host: string = config.host;
+
+  if (!accessKeyId || !host || !secretAccessKey) {
+    throw new Error('accessKeyId/secretAccessKey/host are not set');
+  }
+
+  const awsConfig = new AWS.Config({
+    credentials: new AWS.Credentials(accessKeyId, secretAccessKey),
+    region,
+  });
+
+  const options = {
+    hosts: [host],
+    connectionClass: HttpAmazonESConnector,
+    awsConfig,
+    httpOptions: {}
+  };
+
+  return new elasticsearch.Client(options);
+}
+
+/**
+ * Add a player record to the index. Overwrites existing record, if already
+ * present.
+ */
+export async function addPlayer(
+    client: elasticsearch.Client, id: string, player: Player) {
+  return client.index({
+    index: 'players',
+    type: 'player',
+    id,
+    body: {
+      name: player.name,
+      kitName: player.kitName,
+      abilities: player.abilities,
+      age: player.age,
+    }
+  });
+}
+
+/** Remove a player record from the index. */
+export async function removePlayer(client: elasticsearch.Client, id: string) {
+  return client.delete({
+    index: 'players',
+    type: 'player',
+    id,
+  });
+}
+
+/** Perform basic search. TODO: add more functionality. */
+export async function search(client: elasticsearch.Client, query: string) {
+  return client.search({
+    type: 'player',
+    index: 'players',
+    body: {
+      query: {
+        multi_match: {
+          query,
+          fields: ['name.keyword', 'kitName.keyword'],
+        },
+      },
+    },
+  });
+}
