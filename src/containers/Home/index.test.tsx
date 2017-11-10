4b6fccdc4e00aa0a51f6b486120e60f67bc3dc21
Tim Stirrat <tstirrat@google.com>
2017-11-09 19:09:51 -0800

    Fix Home tests

--- src/containers/Home/index.test.tsx
+++ src/containers/Home/index.test.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
 import { render } from 'react-dom';
+import { StaticRouter } from 'react-router';
 import { Home } from './index';
+
+const context = {};
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   render(
-    <Home />,
+    <StaticRouter location="/" context={context}>
+      <Home />
+    </StaticRouter>,
     div);
 });
