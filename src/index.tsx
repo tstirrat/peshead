cded1ab28fc6e25fca12c64a0e0a1dcbba8b765a
Christos Pappas <christos.pappas@gmail.com>
2018-03-10 18:29:28 -0800

    add material-ui theme (#41)

--- src/index.tsx
+++ src/index.tsx
@@ -6,21 +6,23 @@ import { ConnectedApp } from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import { MuiThemeProvider } from 'material-ui/styles';
 import store, { history } from './configureStore';
+import { createMuiTheme } from 'material-ui/styles';
+import { teal, red, blueGrey } from 'material-ui/colors';
 import './index.css';
 
-// const theme = createMuiTheme({
-//   palette: {
-//     primary: purple, // Purple and green play nicely together.
-//     secondary: {
-//       ...green,
-//       A400: '#00e677',
-//     },
-//     error: red,
-//   },
-// });
+const theme = createMuiTheme({
+  palette: {
+    primary: blueGrey,
+    secondary: {
+      ...teal,
+      A400: '#00e677'
+    },
+    error: red
+  }
+});
 
 ReactDOM.render(
-  <MuiThemeProvider>
+  <MuiThemeProvider theme={theme}>
     <Provider store={store}>
       <ConnectedRouter history={history}>
         <ConnectedApp />
