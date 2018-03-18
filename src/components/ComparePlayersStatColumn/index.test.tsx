cd4a70acbced045e50659e275db557d4799df868
Tim Stirrat <tstirrat@google.com>
2018-03-17 22:04:32 -0700

    feat(compare) clicking player header goes to player info

--- src/components/ComparePlayersStatColumn/index.test.tsx
+++ src/components/ComparePlayersStatColumn/index.test.tsx
@@ -5,10 +5,17 @@ import { base } from '../../__test__/fixtures';
 import { Player } from '../../shared/service/api';
 
 import { ComparePlayersStatColumn } from './index';
+import { StaticRouter } from 'react-router';
 
 const player = Player.create(base);
+const context = {};
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  render(<ComparePlayersStatColumn player={player} />, div);
+  render(
+    <StaticRouter location="/compare/players/1/2" context={context}>
+      <ComparePlayersStatColumn player={player} />
+    </StaticRouter>,
+    div
+  );
 });
