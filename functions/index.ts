c1b26892494fd9be438dcb4916195193d80092bf
Tim Stirrat <tstirrat@google.com>
2018-03-25 19:44:32 -0700

    fix(build) fix old cli/load, remove unused function (#74)

--- functions/index.ts
+++ functions/index.ts
@@ -1,14 +1,8 @@
+// tslint:disable:no-console
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 
-import {
-  addPlayer,
-  createClient,
-  removePlayer,
-  search,
-  suggest
-} from './search';
-import { API, GetPlayerRequest, Player } from './service/api';
+import { addPlayer, createClient, removePlayer, search, suggest } from './search';
 
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
@@ -21,32 +15,6 @@ const CORS_HEADERS_DEV = {
 
 const CORS_HEADERS =
   process.env.NODE_ENV === 'production' ? {} : CORS_HEADERS_DEV;
-
-// API
-
-exports.getPlayer = functions.https.onRequest(async (request, response) => {
-  const api = API.create((method, request, callback) => {
-    callback(null, new Uint8Array([]));
-  });
-  const playerId = '811'; // TODO: get this from url params
-  const req = GetPlayerRequest.create({ playerId });
-  api.getPlayer(req, () => console.log('getPlayer rpc callback'));
-
-  const snapshot = await db
-    .collection('players')
-    .doc(playerId)
-    .get();
-  const player = snapshot.data();
-  console.log('got player from db:', player);
-
-  const rpcPlayer = Player.create({
-    id: playerId
-  });
-  console.log('created rpc player', rpcPlayer.toJSON());
-  response.set(CORS_HEADERS).send(JSON.stringify(player, null, 2));
-});
-
-// SEARCH
 
 enum IndexingState {
   INDEXING = 'INDEXING',
