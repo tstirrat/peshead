45838cfb8d330dc60d4c6154d1e532c42ac244e9
Tim Stirrat <tstirrat@google.com>
2018-04-06 21:03:37 -0700

    refactor(actions) reorder session actions file

--- src/actions/app.ts
+++ src/actions/app.ts
@@ -4,47 +4,15 @@ import { Action } from 'redux';
 import { User } from '../models/user';
 
 export const LOGIN = '[app] LOGIN';
-export const LOGIN_SUCCESS = '[app] LOGIN_SUCCESS';
-export const LOGIN_ERROR = '[app] LOGIN_ERROR';
-export const LOGOUT = '[app] LOGOUT';
-export const LOGOUT_SUCCESS = '[app] LOGOUT_SUCCESS';
-export const LOAD_SESSION = '[app] SESSION_LISTEN';
-
 export class AppLoginAction implements Action {
   public type: typeof LOGIN = LOGIN;
   constructor(public payload: AppLoginPayload) {}
 }
-
 interface AppLoginPayload {
   provider: string;
   id?: string;
   password?: string;
 }
-
-export class AppLoginSuccessAction implements Action {
-  public type: typeof LOGIN_SUCCESS = LOGIN_SUCCESS;
-  constructor(public payload: User) {}
-}
-
-export class AppLoginErrorAction implements Action {
-  public type: typeof LOGIN_ERROR = LOGIN_ERROR;
-  constructor(public payload: Error) {}
-}
-
-export class AppLogoutAction implements Action {
-  public type: typeof LOGOUT = LOGOUT;
-}
-
-export class AppLogoutSuccessAction implements Action {
-  public type: typeof LOGOUT_SUCCESS = LOGOUT_SUCCESS;
-}
-
-export class AppLoadSessionAction implements Action {
-  public type: typeof LOAD_SESSION = LOAD_SESSION;
-}
-
-// Action creators
-
 export const login = (
   provider: string,
   id?: string,
@@ -60,6 +28,11 @@ export const login = (
   };
 };
 
+export const LOGIN_SUCCESS = '[app] LOGIN_SUCCESS';
+export class AppLoginSuccessAction implements Action {
+  public type: typeof LOGIN_SUCCESS = LOGIN_SUCCESS;
+  constructor(public payload: User) {}
+}
 export const loginSuccess = (user: User): AppLoginSuccessAction => {
   return {
     type: LOGIN_SUCCESS,
@@ -67,6 +40,11 @@ export const loginSuccess = (user: User): AppLoginSuccessAction => {
   };
 };
 
+export const LOGIN_ERROR = '[app] LOGIN_ERROR';
+export class AppLoginErrorAction implements Action {
+  public type: typeof LOGIN_ERROR = LOGIN_ERROR;
+  constructor(public payload: Error) {}
+}
 export const loginError = (error: Error): AppLoginErrorAction => {
   return {
     type: LOGIN_ERROR,
@@ -74,18 +52,30 @@ export const loginError = (error: Error): AppLoginErrorAction => {
   };
 };
 
+export const LOGOUT = '[app] LOGOUT';
+export class AppLogoutAction implements Action {
+  public type: typeof LOGOUT = LOGOUT;
+}
 export const logout: ActionCreator<AppLogoutAction> = (): AppLogoutAction => {
   return {
     type: LOGOUT
   };
 };
 
+export const LOGOUT_SUCCESS = '[app] LOGOUT_SUCCESS';
+export class AppLogoutSuccessAction implements Action {
+  public type: typeof LOGOUT_SUCCESS = LOGOUT_SUCCESS;
+}
 export const logoutSuccess = (): AppLogoutSuccessAction => {
   return {
     type: LOGOUT_SUCCESS
   };
 };
 
+export const LOAD_SESSION = '[app] SESSION_LISTEN';
+export class AppLoadSessionAction implements Action {
+  public type: typeof LOAD_SESSION = LOAD_SESSION;
+}
 export const loadSession = (): AppLoadSessionAction => {
   return {
     type: LOAD_SESSION
