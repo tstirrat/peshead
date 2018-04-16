7ef6358933fabb1fb8e37030d5c8f843fb047d5e
Tim Stirrat <tstirrat@google.com>
2018-04-16 16:07:34 -0700

    feat(ui) code split major routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,31 +1,72 @@
+import { CircularProgress } from 'material-ui/Progress';
 import * as React from 'react';
+import { LoadingComponentProps } from 'react-loadable';
+import * as Loadable from 'react-loadable';
+import { pure } from 'recompose';
 import { Fragment, Routes } from 'redux-little-router';
 
 import { NotFound } from './containers/../components/NotFound';
-import { ConnectedComparePlayers } from './containers/ComparePlayers';
-import { ConnectedHome } from './containers/Home';
 import { League } from './containers/League';
-import { ConnectedPlayer } from './containers/Player';
-import { ConnectedSearch } from './containers/Search';
 import { Team } from './containers/Team';
+
+const Loading = pure<LoadingComponentProps>(
+  ({ error, pastDelay }) =>
+    error ? <div>Error!</div> : pastDelay ? <CircularProgress /> : null
+);
+
+const AsyncHome = Loadable({
+  loader: () => import('./containers/Home'),
+  render: (mod, props) => {
+    const Component = mod.ConnectedHome;
+    return <Component {...props} />;
+  },
+  loading: Loading
+});
+
+const AsyncSearch = Loadable({
+  loader: () => import('./containers/Search'),
+  render: (mod, props) => {
+    const Component = mod.ConnectedSearch;
+    return <Component {...props} />;
+  },
+  loading: Loading
+});
+
+const AsyncPlayer = Loadable({
+  loader: () => import('./containers/Player'),
+  render: (mod, props) => {
+    const Component = mod.ConnectedPlayer;
+    return <Component {...props} />;
+  },
+  loading: Loading
+});
+
+const AsyncComparePlayers = Loadable({
+  loader: () => import('./containers/ComparePlayers'),
+  render: (mod, props) => {
+    const Component = mod.ConnectedComparePlayers;
+    return <Component {...props} />;
+  },
+  loading: Loading
+});
 
 export const routes = (
   <Fragment forRoute="/">
     <div>
       <Fragment forRoute="/">
-        <ConnectedHome />
+        <AsyncHome />
       </Fragment>
       <Fragment forRoute="/search">
-        <ConnectedSearch />
+        <AsyncSearch />
       </Fragment>
       <Fragment forRoute="/leagues/:id">
         <League />
       </Fragment>
       <Fragment forRoute="/players/compare/(:player1)(/:player2)(/:player3)">
-        <ConnectedComparePlayers />
+        <AsyncComparePlayers />
       </Fragment>
       <Fragment forRoute="/players/:id">
-        <ConnectedPlayer />
+        <AsyncPlayer />
       </Fragment>
       <Fragment forRoute="/teams/:id">
         <Team />
