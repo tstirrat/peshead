69c76b7eda48398c37b469896c046072d58ff229
Tim Stirrat <tstirrat@google.com>
2018-04-17 12:50:20 -0700

    chore(deps) update redux-beacon

--- src/configureAnalytics.ts
+++ src/configureAnalytics.ts
@@ -1,12 +1,12 @@
+import GoogleAnalytics, { trackPageView } from '@redux-beacon/google-analytics';
 import * as ReactGA from 'react-ga';
 import { createMiddleware } from 'redux-beacon';
-import { GoogleAnalytics, PageView } from 'redux-beacon/targets/google-analytics';
 import { LOCATION_CHANGED, LocationChangedAction } from 'redux-little-router';
 
 import { assert } from './shared/assert';
 
 /** Standard router location change -> pageview */
-const pageView = (action: LocationChangedAction): PageView => {
+const pageView = trackPageView((action: LocationChangedAction) => {
   const location = action.payload;
   const page = location.pathname + location.search;
   const routeExtra = (location.result ? location.result : {}) as {
@@ -18,7 +18,7 @@ const pageView = (action: LocationChangedAction): PageView => {
     page,
     title: `PESto - ${title}`
   };
-};
+});
 
 // Map the event to a Redux action
 const eventsMap = {
@@ -29,5 +29,5 @@ export const analyticsMiddleware = () => {
   const propertyId = `${process.env.REACT_APP_GA_PROPERTY_ID}`;
   assert(propertyId, 'GA property id is missing');
   ReactGA.initialize(propertyId);
-  return createMiddleware(eventsMap, GoogleAnalytics);
+  return createMiddleware(eventsMap, GoogleAnalytics());
 };
